aa65a1b9ea586f22c52bdf8714bd42f532ffd423
David da Silva Contin <dasilvacontin@gmail.com>
2015-07-02 11:54:51 +0200

    send other players position on connect

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 var port = process.env.PORT || 9000
 var io = require('socket.io')(port)
 
+var players = {}
+
 io.on('connection', function (socket) {
   socket.broadcast.emit('hi')
   console.log('connection', socket.id)
+
+  for (var playerId in players) {
+    var playerPos = players[playerId]
+    socket.emit('update_position', playerPos)
+  }
+
   socket.on('disconnect', function () {
     console.log('disconnection', socket.id)
+    delete players[socket.id]
     socket.broadcast.emit('player_disconnected', socket.id)
   })
   socket.on('update_position', function (pos) {
     pos.id = socket.id
+    players[socket.id] = pos
     socket.broadcast.emit('update_position', pos)
   })
 })
