6c436ec87425dfc6f88b44de13a381cdd3248155
pellax <tuxisalive@gmail.com>
2015-07-02 13:43:14 +0200

    añado los objetos a la lista de powerups

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ var io = require('socket.io')(port)
 
 var players = {}
 var powerups={}
+var carrot={}
+
+powerups[id] = carrot
+
 io.on('connection', function (socket) {
   socket.broadcast.emit('hi')
   console.log('connection', socket.id)
@@ -11,16 +15,18 @@ io.on('connection', function (socket) {
     var playerPos = players[playerId]
     socket.emit('update_position', playerPos)
   }
-for(var powerId in powerups){
-var powerPos=powerups[powerId]
-socket.emit('poner_powerups',powerPos)
-}
+  
+  for(var powerId in powerups){
+    var powerPos=powerups[powerId]
+    socket.emit('poner_powerups',powerPos)
+  }
 
   socket.on('disconnect', function () {
     console.log('disconnection', socket.id)
     delete players[socket.id]
     socket.broadcast.emit('player_disconnected', socket.id)
-  })
+
+ })
   socket.on('update_position', function (pos) {
     pos.id = socket.id
     players[socket.id] = pos
